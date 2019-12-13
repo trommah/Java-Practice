dcdf130ca81aba4595ff69ffb8dee86e34b625c2
trommah <37719881+trommah@users.noreply.github.com>
2019-12-13 09:32:20 -0600

    Copied My Solution on GFG

--- Java-Practice/src/codingChallenges/DynamicProgramming/GoldMine.java
+++ Java-Practice/src/codingChallenges/DynamicProgramming/GoldMine.java
@@ -4,38 +4,60 @@ package codingChallenges.DynamicProgramming;
 //at first column but can be at any row. He can move only (right->,right up /,right down\) that is from a given cell,
 //the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right. 
 //Find out maximum amount of gold he can collect.
-public class GoldMine {
 
-	public static void main(int[][] gold, int i, int j) {		//gold contains the gold at a location, (i,j)
-		GoldMine G = new GoldMine();
-		G.maxGold(gold, i, j);
-	}
-	
-	public int maxGold(int[][] gold, int i, int j) //finds the max gold attained from the starting point with the miner's defined movements
-	{
-		int[][] maximizedGold = new int[gold.length][gold[0].length];
-		for(int k = 0; k < maximizedGold.length; k++)
-		{
-			for(int l = 0; l < maximizedGold[0].length; l++)
-			{
-				maximizedGold[k][l] = -1;
-			}
-		}
-		
-		int mostGold = findGold(gold, i, j, maximizedGold);
-		return mostGold;
-	}
-	
-	public int findGold(int[][] gold, int i, int j, int[][] maximizedGold)
-	{
-		int q;
-		if(i < 0 || j < 0 || i > gold.length || j > gold[0].length)
-			return 0;
-		if(maximizedGold[i][j] >= 0)
-			return maximizedGold[i][j];
-		q = gold[i][j] + Math.max(Math.max(findGold(gold, i - 1, j + 1, maximizedGold), findGold(gold, i, j + 1, maximizedGold)), findGold(gold, i + 1, j + 1, maximizedGold));
-		maximizedGold[i][j] = q;
-		return q;
-	}
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class GFG {
+	public static void main (String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 int t=s.nextInt();
+		 int[][] gold;
+		 int n, m;
+		 
+		 for(int i=0;i<t;i++)
+		 {
+		    int n = s.nextInt();
+		    int m = s.nextInt();
+		    Scanner lineScanner = new Scanner(s.nextLine());
+		    for(int j = 0; j < n; j++)
+		        for(int k = 0; k < m; k++)
+		        {
+		            gold[j][k] = lineScanner.nextInt();
+		        }
+		        System.out.print(findGold(gold));
+		 }
+		 
+	}
+}
+
+public int findGold(int[][] gold)
+{
+    int[][] maxGold = new int[gold.length][gold[0].length];
+    for(int l = 0; l < gold.length; l++)
+    {
+        for(int k = 0; k < gold[0].length; k++)
+        {
+            maxGold[l][k] = -1;
+        }
+    }
+    int maximumGold = 0;
+    for(int i = 0; i < gold.length; i++)
+    {
+        maximumGold = Math.max(maximumGold, maximizedGold(gold, i, 0, maxGold);
+    }
+    return maximumGold;
+}
+
+public int maximizedGold(int[][] gold, int i, int j, int[][] maxGold)
+{
+    int q = 0;
+    if(i < 0 || j < 0 || i > gold.length - 1 || j > gold[0].length - 1)
+        return q;
+    if(maxGold[i][j] > 0)
+        return maxGold[i][j];
+    q = Math.max(maximizedGold(gold, i - 1, j + 1, maxGold), maximizedGold(gold, i, j + 1, maxGold),                                     maximizedGold(gold, i + 1, j + 1, maxGold));
+    maxGold[i][j] = q;
+    return q;
 }
